48b6b90e02e2ac65d127c037d9c8fc53ccb0852f
Matt <wrestler.411@comcast.net>
2017-12-20 11:45:47 -0800

    Trying to set up loop for 26 draws and deck refill

--- war_card_game.cs/war_card_game.cs/Program.cs
+++ war_card_game.cs/war_card_game.cs/Program.cs
@@ -2,11 +2,37 @@
 
 class War
 {
-    static int[] deck = new int[52]; 
-    static string[] cards = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
-    static string[] suits = { "clubs", "diamonds", "hearts", "spades" };
-    
+    static int[] deck = new int[52]; //using logic that 1-52 is all cards, with Ace of clubs, diamonds, hearts, and spades as 
+    static string[] cards = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+    static string[] suits = { "hearts", "diamonds", "clubs", "spades" };
+    static Random rand = new Random();
 
     static void Main()
     {
-        
+        int totalPlayer1 = 0;
+        int totalPlayer2 = 0;
+
+        for (int i = 0; i < 26; i++)
+        {
+
+        }
+    }
+
+    static void FillDeck()
+    {
+        for (int i = 0; i < 52; i++)
+        {
+            while (true)
+            {
+                int num = rand.Next(1, 53);
+                if (Array.IndexOf(deck, num) == -1)
+                {
+                    deck[i] = num;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
