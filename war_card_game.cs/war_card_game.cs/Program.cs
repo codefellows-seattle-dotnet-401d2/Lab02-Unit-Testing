7504eeadc10834afd024ebd884b65b30159b2405
Matt <wrestler.411@comcast.net>
2017-12-20 12:06:22 -0800

    Starting turn loop logic and drawcard function

--- war_card_game.cs/war_card_game.cs/Program.cs
+++ war_card_game.cs/war_card_game.cs/Program.cs
@@ -2,7 +2,8 @@
 
 class War
 {
-    static int[] deck = new int[52]; //using logic that 1-52 is all cards, with Ace of clubs, diamonds, hearts, and spades as 
+    static int[] deck = new int[52]; //using logic that 1-52 is all cards, with 2 of clubs, diamonds, hearts, and spades as 1-4
+    static int lastCardIndex = 0;
     static string[] cards = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
     static string[] suits = { "hearts", "diamonds", "clubs", "spades" };
     static Random rand = new Random();
@@ -14,7 +15,10 @@ class War
 
         for (int i = 0; i < 26; i++)
         {
-
+            string player1Suit;
+            int player1 = SelectCard(out player1Suit) - 1;
+            string player2Suit;
+            int player = SelectCard(out player2Suit) - 1;
         }
     }
 
@@ -32,6 +36,14 @@ class War
                 }
             }
         }
+    }
+
+    static int SelectCard(out string suit)
+    {
+        int num = deck[lastCardIndex];
+        lastCardIndex++;
+        suit = suits[(num - 1) % 4];
+        return (num - 1) / 4 + 1;
     }
 }
 
