931ff7bc1bc904c574404b4ce090d49c42d15dcb
Matt <wrestler.411@comcast.net>
2018-01-09 09:44:23 -0800

    codecracker code, needs xunit tests

--- codecracker.cs/codecracker.cs/Program.cs
+++ codecracker.cs/codecracker.cs/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 
-namespace codecracker.cs
+namespace CodeCracker
 {
-    class Program
+    public class GettingCryptic
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Encrypt: Trying things");
+            Console.WriteLine(Encrypt("Trying things"));
+
+            Console.WriteLine("Decrypt: >*bbe");
+            Console.WriteLine(Decrypt(">*bbe"));
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Given a string of characters, encode into a secret message using a built in key.
+        /// </summary>
+    
+        public static string Encrypt(string words)
+        {
+            string encryptMessage = "";
+            
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] code = { '!', ')', '"', '(', '£', '*', '%', '&', '>', '<', '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o' };
+           
+
+            foreach (char c in plainText.ToLower())
+            {
+                int index = Array.IndexOf(alphabet, c);
+                if (index != -1) encryptMessage += code[index];
+                else encryptMessage += " ";
+            }
+            return encryptMessage;
+        }
+
+        /// <summary>
+        /// Given a string of characters, decode secret message into plain text using built in key.
+        /// </summary>
+   
+        public static string Decrypt(string encryptMessage)
+        {
+            string words = "";
+
+            char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+            char[] code = {'!',')','"','(','£','*','%','&','>','<','@','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'};
+
+            foreach (char c in encryptMessage.ToLower())
+            {
+                int index = Array.IndexOf(code, c);
+                if (index != -1) words += alphabet[index];
+                else plainText += " ";
+            }
+            return words;
         }
     }
 }
